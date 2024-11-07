82535bc4e490b2f834271e72a9324e2c24e09721
LesiaP92 <lesja_p92@icloud.com>
2024-11-07 16:19:08 +0200

    test

---
+++ script/scripts.js
@@ -0,0 +1,12 @@
+var str;
+str = 'HELLO';
+var str1;
+str1 = 'WORLD';
+window.alert(str);
+window.alert(str1);
+
+var worldId;
+var worldElement;
+worldElement = document.getElementById(worldId);
+worldId = 'world';
+window.alert(worldElement.value);

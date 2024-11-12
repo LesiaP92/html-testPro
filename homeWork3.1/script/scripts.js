095665604e182e5546fa60ddcb35720b002819b4
LesiaP92 <lesja_p92@icloud.com>
2024-11-12 22:13:40 +0200

    homeWork 3.2

--- homeWork3.1/script/scripts.js
+++ homeWork3.1/script/scripts.js
@@ -1,6 +1,6 @@
 console.log("string:", typeof "Hello, Serhey!");                //string
 console.log("number:", typeof "29");                            //number
 console.log("bigint:", typeof "3087545575789n");                //bigint
-console.log("symbol:", typeof Symbol("Registration")) //symbol
+console.log("symbol:", typeof Symbol("Registration")); //symbol
 console.log("object (null):", typeof "null");                   //object (не відоме значення)
 console.log("boolean:", typeof "false");                        //boolean
---
+++ homeWork3.2/script2.js
@@ -0,0 +1,4 @@
+const message1 = prompt("How are you?");
+const message2 = prompt("How is your mood?");
+const message3 = prompt("What do you do?");
+console.log(`${message1}, ${message2}, ${message3}`);

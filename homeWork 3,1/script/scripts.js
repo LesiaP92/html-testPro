2b2fa3a8c9b244103a33413422315161f2a87b42
LesiaP92 <lesja_p92@icloud.com>
2024-11-12 15:25:12 +0200

    homeWork 3.1

---
+++ homeWork 3,1/script/scripts.js
@@ -0,0 +1,6 @@
+console.log("string:", typeof "Hello, Serhey!");                //string
+console.log("number:", typeof "29");                            //number
+console.log("bigint:", typeof "3087545575789n");                //bigint
+console.log("boolean:", typeof "false");                        //boolean
+console.log("symbol:", typeof Symbol("Registration")) //symbol
+console.log("object (null):", typeof "null");                   //object (не відоме значення)

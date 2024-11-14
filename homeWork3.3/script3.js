9dfce52fb6bcc836c0d718252137a555e1e3ba2c
LesiaP92 <lesja_p92@icloud.com>
2024-11-14 15:35:11 +0200

    homework3.3

---
+++ homeWork3.3/script3.js
@@ -0,0 +1,4 @@
+const number = 12345;
+const numberStr = String(number);
+const result = numberStr[0] + ' ' + numberStr[1] + ' ' + numberStr[2] + ' ' + numberStr[3] + ' ' + numberStr[4];
+console.log(result);

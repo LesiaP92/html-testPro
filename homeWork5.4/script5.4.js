5b33633f58da461f063908322b1a6087449e28af
LesiaP92 <lesja_p92@icloud.com>
2024-11-28 15:21:03 +0200

    homeWork5.4

--- homeWork5.4/script5.4.js
+++ homeWork5.4/script5.4.js
@@ -1,18 +1,23 @@
-while (true){
-    const input = prompt("Введіть ціле число:");
-    if (input === null || input.trim() === ""){
+while (true) {
+    const input = prompt("Введіть число:");
+
+    if (input === null || input.trim() === "") {
         alert("Програму завершено.");
         break;
     }
-    const N = parseInt(input);
-    if (isNaN(N)) {
-        alert("Не вірно.Спробуйте ще раз.");
+
+    const N = Number(input);
+
+    if (isNaN(N) || !Number.isInteger(N)) {
+        alert("Спробуйте ще раз.");
         continue;
-}
+    }
+
     if (N <= 1) {
         alert(`${N} не є простим числом.`);
     } else {
         let isPrime = true;
+
         for (let i = 2; i < N; i++) {
             if (N % i === 0) {
                 isPrime = false;
@@ -20,16 +25,17 @@ while (true){
             }
         }
 
+        if (isPrime) {
+            alert(`${N} є простим числом.`);
+        } else {
+            alert(`${N} не є простим числом.`);
+        }
+    }
 
+    const continueCalc = confirm("Хочете обчислити ще одне число?");
+    if (!continueCalc) {
+        alert("Програму завершено.");
+        break;
     }
 }
-if (isPrime) {
-    alert(`${N} є простим числом.`);
-} else {
-    alert(`${N} не є простим числом.`);
-}
-const continueCalc = confirm("Хочете обчислити ще одне число?");
-if (!continueCalc) {
-    alert("Програму завершено.");
 
-}

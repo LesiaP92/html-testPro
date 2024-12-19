54470b370ad66ccae087c958ba18e1eaae3f0a1f
LesiaP92 <lesja_p92@icloud.com>
2024-12-19 14:57:49 +0200

    homeWork12.1

---
+++ homeWork12.1/script12.1.js
@@ -0,0 +1,18 @@
+let userLink = '';
+
+document.getElementById('setLinkButton').addEventListener('click', function() {
+    userLink = prompt('Введіть посилання (URL):', 'https://');
+    if (userLink) {
+        alert('Посилання збережено!');
+    } else {
+        alert('Спробуй ще! Посилання не збережено.');
+    }
+});
+
+document.getElementById('redirectButton').addEventListener('click', function() {
+    if (userLink) {
+        window.location.href = userLink;
+    } else {
+        alert('Спочатку введіть посилання!');
+    }
+});
